8884edc04e8c949c0b48e1b85f5a4ec1bf73d5af
halilagin <halil.agin@gmail.com>
2023-08-18 16:03:52 +0000

    datapipelines-028 added

--- datapipelines-028/py_src/.ipynb_checkpoints/get_minio_url-checkpoint.py
+++
@@ -1,8 +0,0 @@
-#%load py_src/get_minio_url.py
-#%%file
-import os
-def get_minio_url():
-    minio_host, minio_port = os.environ["MINIO_SERVICE_SERVICE_HOST"], os.environ["MINIO_SERVICE_SERVICE_PORT_HTTP"]
-    minio_url= "{}:{}".format(minio_host, minio_port)
-    return minio_url
-
--- datapipelines-028/py_src/compile_model.py
+++
@@ -1,7 +0,0 @@
-#%load py_src/compile_model.py
-#%%file
-#compile the model - we want to have a multiple outcome
-def compile_model(_model):
-    _model.compile(optimizer="adam",
-                  loss="sparse_categorical_crossentropy",
-                  metrics=['accuracy'])
